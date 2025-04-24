a467b32838ba3c64dcd53db44207ee29c40c1e0e
Haikal <haikal@Haikals-MacBook-Air.local>
2025-04-24 09:51:43 +0700

    perbaikan font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        badscript: ['"Bad Script"', 'cursive'],
+        inter: ['"Inter"', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
-
